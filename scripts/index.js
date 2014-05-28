91bec73988515bbf417290eedb5c80e052c97559
rohanbhangui <bhanguirohan@gmail.com>
2014-05-27 22:27:14 -0400

    apple iphone5s site style nav

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,65 @@
 $(document).ready(function () {
 
-  var navArr = [];
-  $("#main-menu li").each(function(index) {
-    navArr.push($(this).text());
-    $(this).text("");
+  /*$("#main-menu li").on("mouseenter", function() {
+    $(this).children("span.nav-text").fadeIn();
   });
 
-  console.log(navArr);
-
-  $("#main-menu li").on("hover", function() {
-    console.log($(this));
-    $("#main-menu li").text("");
-    $(this).text(navArr[$(this).index()]);
+  $("#main-menu li").on("mouseleave", function() {
+    $("#main-menu li span.nav-text").fadeOut();
+  });
+*/
+  
+  $(window).on("load", function() {
+    $(window).scrollTop(0);
   });
 
-  $("#main-menu").on("mouseleave", function() {
-    $("#main-menu li").text("");
+  var currentScreen = $("#main-menu li:first-child");
+
+  var currentlyScrolling = false;
+
+  $(window).on("mousewheel", function() {
+
+    $("#main-menu li").removeClass("active");
+
+    if(event.deltaY > 0 && currentScreen.attr("data-link") != "contact")
+    {
+      if(!currentlyScrolling)
+      {
+        currentlyScrolling = true;
+        $('html, body').stop().animate({
+          scrollTop: $("#" + currentScreen.attr("data-link")).offset().top
+        }, 1000, function() {
+          currentlyScrolling = false;
+        });
+      }
+
+      currentScreen = currentScreen.next();
+    }
+    else if (event.deltaY < 0 && currentScreen.attr("data-link") != "splash")
+    {
+      if(!currentlyScrolling)
+      {
+        currentlyScrolling = true;
+        $('html, body').stop().animate({
+          scrollTop: $("#" + currentScreen.attr("data-link")).offset().top
+        }, 1000, function() {
+          currentlyScrolling = false;
+        });
+      }
+
+      currentScreen = currentScreen.prev();
+    }
+
+    currentScreen.addClass("active");
+
+    console.log(currentScreen.attr("data-link"));
   });
 
+  $("#main-menu li").on("click", function() {
+    $("#main-menu li").removeClass("active");
+    $(this).addClass("active");
+    $('html, body').stop().animate({
+        scrollTop: $("#" + $(this).attr("data-link")).offset().top
+    }, 1000);
+  });
 });
